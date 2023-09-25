98921797b4a7253517564a7c42552215ac16dc2c
abhideveloping <abhinandantiwari150@gmail.com>
2023-09-25 19:23:21 +0530

    yeah so time to add this one

---
+++ functiontest.py
@@ -0,0 +1,10 @@
+def work(animal_type, pet_name):
+	"""Display information about a pet."""
+	print("\nI have a " +animal_type + ".")
+	print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+work('hamster', 'harry')
+work('dog', 'willie')
+
+
+		
